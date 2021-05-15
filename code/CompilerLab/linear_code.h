0426b386bf0d0aee8aaa86c470a0ff3c2b4646d4
Chenqi <1002652154@qq.com>
2021-05-15 15:04:27 +0800

    linear code definition updated

--- code/CompilerLab/linear_code.h
+++ code/CompilerLab/linear_code.h
@@ -9,53 +9,149 @@ using std::vector;
 
 
 enum class LineType : uchar {
-	Operation,	// op x0 x1 x2
-	Load,		// ld x0 x1 x2
-	Store,		// st x0 x1 x2
-	Parameter,	// pm x0
-	FuncCall,	// cl f0
-	Label,		// lb l0
-	JumpIf,		// ji l0 x1   // use op or not?
-	Goto,		// gt l0
-	Return		// rt x0
+	Operation,	//	op	x0	x1	x2
+	Load,		//	ld	x0	x1	x2
+	Store,		//	st	x0	x1	x2
+	Parameter,	//	pm	x0
+	FuncCall,	//	cl	f0  x1
+	Label,		//	lb	l0
+	JumpIf,		//	jo	l0	x1	x2
+	Goto,		//	gt	l0
+	Return,		//	rt	x0
 };
 
 
-struct VarIndex {
+enum class VarType : uchar {
+	Empty,
+	Global,
+	Local,
+	Number
+};
+
+
+struct VarInfo {
+private: friend class CodeLine;
+	VarType type;
+	int value;
 private:
-	ushort index;
+	constexpr VarInfo() : type(VarType::Empty), value() {}
 public:
-	VarIndex() : index(-1) {}
-	VarIndex(ushort index, bool is_global) : index((index << 1) | is_global) {}
-	bool IsValid() const { return index != -1; }
-	bool IsGlobal() const { return index & 1; }
-	ushort GetIndex() const { return index >> 1; }
-	ushort GetRawIndex() const { return index; }
+	explicit constexpr VarInfo(VarType type, int value) : type(type), value(value) {
+		assert(type != VarType::Empty && type <= VarType::Number);
+		if (type == VarType::Global || type == VarType::Local) { assert(value >= 0); }
+	}
 };
-
-using FuncIndex = ushort;
-using LabelIndex = ushort;
 
 
 struct CodeLine {
-	LineType type;
-	OperatorType op = OperatorType::None;
-	union {
-		VarIndex var0;
-		FuncIndex func;
-		LabelIndex label;
-	};
-	VarIndex var1;
-	VarIndex var2;
+private: friend class CodeLinePrinter;
+	LineType type : 4;
+	OperatorType op : 4;
+	VarType var_type[3];
+	int var[3];
+
+private:
+	CodeLine(LineType type) : type(type), op(OperatorType::None), var_type(), var() {}
+
+private:
+	void SetVar(VarInfo var0, VarInfo var1, VarInfo var2) {
+		var_type[0] = var0.type; var[0] = var0.value;
+		var_type[1] = var1.type; var[1] = var1.value;
+		var_type[2] = var2.type; var[2] = var2.value;
+	}	
+	static CodeLine Operation(OperatorType op, VarInfo dest, VarInfo src1, VarInfo src2) {
+		CodeLine code_line(LineType::Operation); 
+		code_line.op = op;
+		code_line.SetVar(dest, src1, src2);
+		return code_line;
+	}
+
+public:
+	static CodeLine BinaryOperation(OperatorType op, VarInfo dest, VarInfo src1, VarInfo src2) {
+		assert(IsBinaryOperator(op));
+		assert(dest.type != VarType::Number);
+		return Operation(op, dest, src1, src2);
+	}
+	static CodeLine UnaryOperation(OperatorType op, VarInfo dest, VarInfo src) {
+		assert(IsUnaryOperator(op));
+		assert(dest.type != VarType::Number);
+		return Operation(op, dest, src, {});
+	}
+	static CodeLine Load(VarInfo dest, VarInfo src_begin, VarInfo src_offset) {
+		assert(dest.type != VarType::Number);
+		assert(src_begin.type != VarType::Number);
+		CodeLine code_line(LineType::Load);
+		code_line.SetVar(dest, src_begin, src_offset);
+		return code_line;
+	}
+	static CodeLine Store(VarInfo src, VarInfo dest_begin, VarInfo dest_offset) {
+		assert(dest_begin.type != VarType::Number);
+		CodeLine code_line(LineType::Store);
+		code_line.SetVar(src, dest_begin, dest_offset);
+		return code_line;
+	}
+	static CodeLine Parameter(VarInfo para) {
+		CodeLine code_line(LineType::Parameter);
+		code_line.SetVar(para, {}, {});
+		return code_line;
+	}
+	static CodeLine IntFuncCall(int func_index, VarInfo dest) {
+		assert(func_index >= 0);
+		assert(dest.type != VarType::Number);
+		CodeLine code_line(LineType::FuncCall);
+		code_line.var[0] = func_index; 
+		code_line.var_type[1] = dest.type; code_line.var[1] = dest.value;
+		return code_line;
+	}
+	static CodeLine VoidFuncCall(int func_index) {
+		assert(func_index >= 0);
+		CodeLine code_line(LineType::FuncCall);
+		code_line.var[0] = func_index;
+		return code_line;
+	}
+	static CodeLine Label(int label_index) {
+		assert(label_index >= 0);
+		CodeLine code_line(LineType::Label);
+		code_line.var[0] = label_index;
+		return code_line;
+	}
+	static CodeLine JumpIf(int label_index, OperatorType op, VarInfo src1, VarInfo src2) {
+		assert(label_index >= 0);
+		assert(op >= OperatorType::Equal && op <= OperatorType::GreaterEuqal);
+		CodeLine code_line(LineType::JumpIf);
+		code_line.op = op;
+		code_line.SetVar({}, src1, src2);
+		code_line.var[0] = label_index;
+		return code_line;
+	}
+	static CodeLine JumpIf(int label_index, VarInfo src) {
+		return JumpIf(label_index, OperatorType::Greater, src, VarInfo(VarType::Number, 0));
+	}
+	static CodeLine Goto(int label_index) {
+		assert(label_index >= 0);
+		CodeLine code_line(LineType::Goto);
+		code_line.var[0] = label_index;
+		return code_line;
+	}
+	static CodeLine ReturnVoid() {
+		CodeLine code_line(LineType::Return);
+		return code_line;
+	}
+	static CodeLine ReturnInt(VarInfo var) {
+		CodeLine code_line(LineType::Return);
+		code_line.var_type[0] = var.type; code_line.var[0] = var.value;
+		return code_line;
+	}
 };
-static_assert(sizeof(CodeLine) == 8);
+static_assert(sizeof(CodeLine) == 16);
+
 
 using CodeBlock = vector<CodeLine>;
 
 
 struct GlobalVarDef {
 	uint length;
-	vector<std::pair<uint, int>> initializer_list;
+	vector<std::pair<uint, int>> initializing_list;
 };
 
 using GlobalVarTable = vector<GlobalVarDef>;
--- code/CompilerLab/linear_code_printer.h
+++ code/CompilerLab/linear_code_printer.h
@@ -9,63 +9,87 @@ using std::cout;
 using std::endl;
 
 
-static std::ostream& operator<<(std::ostream& os, VarIndex var_index) {
-	assert(var_index.IsValid());
-	return os << (var_index.IsGlobal() ? "G" : "l") << var_index.GetIndex();
+static std::ostream& operator<<(std::ostream& os, std::pair<VarType, int> var_info) {
+	switch (var_info.first) {
+	case VarType::Global: return os << "g" << var_info.second;
+	case VarType::Local: return os << "l" << var_info.second;
+	case VarType::Number: return os << var_info.second;
+	default: assert(false); return os;
+	}
 }
 
 
-class LinearCodePrinter {
+class CodeLinePrinter {
+private:
+	static std::pair<VarType, int> VarInfo(const CodeLine& line, int index) {
+		assert(index >= 0 && index < 3);
+		return { line.var_type[index],  line.var[index] };
+	}
 public:
-	void PrintLinearCode(const LinearCode& linear_code) {
-		uint counter;
+	static void PrintCodeLine(const CodeLine& line) {
+		switch (line.type) {
+		case LineType::Operation:
+			line.var_type[2] == VarType::Empty ?
+				cout << '\t' << VarInfo(line, 0) << " = " << GetOperatorString(line.op) << VarInfo(line, 1) << endl:
+				cout << '\t' << VarInfo(line, 0) << " = " << VarInfo(line, 1) << " " << GetOperatorString(line.op) << " " << VarInfo(line, 2) << endl;
+			break;
+		case LineType::Load:
+			cout << "\t" << VarInfo(line, 0) << " = " << VarInfo(line, 1) << "[" << VarInfo(line, 2) << "]" << endl;
+			break;
+		case LineType::Store:
+			cout << "\t" << VarInfo(line, 1) << "[" << VarInfo(line, 2) << "]" << " = " << VarInfo(line, 0) << endl;
+			break;
+		case LineType::Parameter:
+			cout << "\tparam " << VarInfo(line, 0) << endl;
+			break;
+		case LineType::FuncCall:
+			line.var_type[1] == VarType::Empty ? cout : cout << VarInfo(line, 1) << " = ";
+			cout << "\tcall f" << line.var[0] << endl;
+			break;
+		case LineType::Label:
+			cout << "label" << line.var[0] << ":" << endl;
+			break;
+		case LineType::JumpIf:
+			cout << "\tif " << VarInfo(line, 1) << GetOperatorString(line.op) << VarInfo(line, 2) << " goto label" << line.var[0] << endl; 
+			break;
+		case LineType::Goto:
+			cout << "\tgoto label" << line.var[0] << endl;
+			break;
+		case LineType::Return:
+			cout << "\treturn"; 
+			line.var_type[0] == VarType::Empty ? cout << endl : cout << VarInfo(line, 0) << endl;
+			break;
+		default:
+			assert(false);
+			return;
+		}
+	}
+};
 
-		// print global var
-		counter = 0;
-		for (auto& global_var : linear_code.global_var_table) {
-			cout << "G" << counter++ << ": " << global_var.length << endl;
-			for (auto& [index, val] : global_var.initializer_list) {
+
+class LinearCodePrinter {
+private:
+	void PrintGlobalVar(const GlobalVarTable& global_var_table) {
+		uint counter = 0;
+		for (auto& global_var : global_var_table) {
+			cout << "g" << counter++ << ": " << global_var.length << endl;
+			for (auto& [index, val] : global_var.initializing_list) {
 				cout << "\t[" << index << "] " << val << endl;
 			}
 		}
-
-		// print global func
-		counter = 0;
-		for (auto& global_func : linear_code.global_func_table) {
-			cout << "Func" << counter++ << ": " << global_func.parameter_count << " " << global_func.local_var_count << endl;
+	}
+	void PrintGlobalFunc(const GlobalFuncTable& global_func_table) {
+		uint counter = 0;
+		for (auto& global_func : global_func_table) {
+			cout << "func" << counter++ << ": " << global_func.parameter_count << " " << global_func.local_var_count << endl;
 			for (auto& line : global_func.code_block) {
-				switch (line.type) 	{
-				case LineType::Operation: 
-					cout << "\t" << line.var0 << " = " << line.var1 << " " << GetOperatorString(line.op) << " " << line.var2 << endl; 
-					break;
-				case LineType::Load: 
-					cout << "\t" << line.var0 << " = " << line.var1 << "[" << line.var2 << "]" << endl;
-					break;
-				case LineType::Store: 
-					cout << "\t" << line.var1 << "[" << line.var2 << "]" << " = " << line.var0 << endl;
-					break;
-				case LineType::Parameter: 
-					cout << "\tparam " << line.var0 << endl;
-					break;
-				case LineType::FuncCall: 
-					cout << "\tcall f" << line.func << endl;
-					break;
-				case LineType::Label: 
-					cout << "label" << line.label << ":" << endl;
-					break;
-				case LineType::JumpIf: 
-					cout << "\tif " << line.var1 << " goto label" << line.label << endl; break;
-				case LineType::Goto: 
-					cout << "\tgoto label" << line.label << endl; 
-					break;
-				case LineType::Return: 
-					cout << "\treturn"; line.var0.IsValid() ? cout << line.var0 << endl : cout << endl;
-					break;
-				default: 
-					assert(false); 
-					return;
-				}
+				CodeLinePrinter::PrintCodeLine(line);
 			}
 		}
+	}
+public:
+	void PrintLinearCode(const LinearCode& linear_code) {
+		PrintGlobalVar(linear_code.global_var_table);
+		PrintGlobalFunc(linear_code.global_func_table);
 	}
 };
